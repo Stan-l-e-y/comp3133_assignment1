19a888e896655dae89402c4558fd95a7cda3b196
Stanley Tsonev <stanleytsonev@hotmail.com>
2023-04-05 08:01:56 -0400

    good

--- dist/src/server.js
+++ dist/src/server.js
@@ -7,16 +7,20 @@ import bodyParser from 'body-parser';
 import { resolvers } from '../graphql/resolvers.js';
 import { readFileSync } from 'fs';
 import { DIRECTIVES } from '@graphql-codegen/typescript-mongodb';
-const typeDefs = readFileSync('./graphql/schema.graphql', { encoding: 'utf-8' });
+const typeDefs = readFileSync('./graphql/schema.graphql', {
+    encoding: 'utf-8',
+});
 const server = new ApolloServer({
     typeDefs: [typeDefs, DIRECTIVES],
     resolvers,
 });
 await server.start();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const MONGODB_URI = 'mongodb+srv://ThisIsForSchool:lSaFPLvHIk2bPCCz@cluster0.mtqaf6e.mongodb.net/test?retryWrites=true&w=majority';
 const app = express();
 app.use('/', cors(), bodyParser.json(), expressMiddleware(server));
+app.use(cors({ origin: '*', credentials: true }));
+console.log('test');
 try {
     const mongoConnect = async () => {
         await connect(MONGODB_URI);
@@ -25,7 +29,7 @@ try {
     // app.use(express.json());
     // app.listen(PORT, () => console.log(`listening on port ${PORT}`));
     await new Promise((resolve) => app.listen({ port: PORT }, resolve));
-    console.log(`Server ready at http://localhost:${PORT}/`);
+    console.log(`Server ready at chicken wing http://localhost:${PORT}/`);
 }
 catch (err) {
     console.error(err);
